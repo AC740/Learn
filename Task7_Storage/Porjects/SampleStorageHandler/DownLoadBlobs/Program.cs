c10d2852a224162773f85b0cf7e502d48e8892d4
Andrew Choi <andrew@mediabrix.com>
2018-04-04 19:33:13 -0400

    Second

---
+++ Task7_Storage/Porjects/SampleStorageHandler/DownLoadBlobs/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DownLoadBlobs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure; // Namespace for CloudConfigurationManager
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("BlobStorageConnectionString"));
+            // Create the table client.
+            CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+            // Retrieve a reference to the table.
+
+            var cloudBlobs = cloudBlobClient.GetContainerReference("sampleblobcontainer").GetDirectoryReference("SampleData").ListBlobs();
+
+            foreach (CloudBlockBlob blob in cloudBlobs)
+            {
+                var test = blob.Name;
+                using (var fileStream = System.IO.File.Create(@"\GitHub\Learn\Task7_Storage\Porjects\BlobDataDownLoad\" + blob.Name))
+                {
+                    var blobReference = cloudBlobClient.GetBlobReferenceFromServer(blob.Uri);
+                    blobReference.DownloadToStream(fileStream);
+                }
+            }
+
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+    }
+}
--- Task7_Storage/Porjects/SampleStorageHandler/SampleStorageHandler/Program.cs
+++ Task7_Storage/Porjects/SampleStorageHandler/SampleStorageHandler/Program.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Azure; // Namespace for CloudConfigurationManager
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
-//using Microsoft.Azure.Storage; // Namespace for StorageAccounts
-//using Microsoft.Azure.CosmosDB.Table; // Namespace for Table storage types
-
-namespace SampleStorageHandler
+﻿namespace SampleStorageHandler
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure; // Namespace for CloudConfigurationManager
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Table;
+
     /// <summary>
     /// Download Table Content and display all records on screen
     /// </summary>
