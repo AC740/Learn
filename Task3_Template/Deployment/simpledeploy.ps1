0deaef649ad06cb1de8c8f153d2a1863e7bfae3a
Andrew Choi <andrew@mediabrix.com>
2018-04-05 07:17:44 -0400

    04052018

---
+++ Task3_Template/Deployment/simpledeploy.ps1
@@ -0,0 +1,26 @@
+﻿### Define Deployment Variables
+{
+$location = 'EAST US'
+$resourceGroupName = 'Andrew-Test-Grp-Copy'
+$resourceDeploymentName = 'andrewtestgrpcopydeploy'
+$templatePath = $env:SystemDrive + '\' + 'GitHub\Learn\Task7_Storage\test'
+$templateFile = 'template.json'
+$template = $templatePath + '\' + $templateFile
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
